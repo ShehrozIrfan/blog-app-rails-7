e730173ee6a6a6fb94a5f89557cdf8c9bdaddbf6
Shehroz Irfan <shehrozirfan89@gmail.com>
2023-06-19 11:55:03 +0500

    Tests added for the blog post model methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # assert -> it expectst the true
+  # refute -> it expects the false
+
+  test "draft? returns true for the draft blog post" do
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test "draft? returns false for the published blog post" do
+    refute BlogPost.new(published_at: 1.year.ago).draft?
+  end
+
+  test "draft? returns false for the scheduled blog post" do
+    refute BlogPost.new(published_at: 1.year.from_now).draft?
+  end
+
+  test "published? returns true for the published blog post" do
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+
+  test "published? returns false for the draft blog post" do
+    refute BlogPost.new(published_at: nil).published?
+  end
+
+  test "published? returns false for the scheduled blog post" do
+    refute BlogPost.new(published_at: 1.year.from_now).published?
+  end
+
+  test "scheduled? returns true for the scheduled blog post" do
+    assert BlogPost.new(published_at: 1.year.from_now).scheduled?
+  end
+
+  test "scheduled? returns false for the draft blog post" do
+    refute BlogPost.new(published_at: nil).scheduled?
+  end
+
+  test "scheduled? returns false for the published blog post" do
+    refute BlogPost.new(published_at: 1.year.ago).scheduled?
+  end
 end
